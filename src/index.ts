27ab577f8c1be9a2b1a6e99ee4c239be356ec6a5
Ivan Galvagno <ivan.galvagno@gmail.com>
2021-07-04 13:44:33 -0300

    Install dependencies and basic start

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,18 @@ import {createConnection} from "typeorm";
 import * as express from "express";
 import * as bodyParser from "body-parser";
 import {Request, Response} from "express";
-import {Routes} from "./routes";
+import * as cors from 'cors';
+import routes from "./routes";
+import * as dotenv from 'dotenv';
+
+const app = express();
+const PORT = 3333;
+app.use(cors());
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(routes);
+
+
+app.listen(PORT, () =>{
+    console.log(`Api started at the port: ${PORT}`);
+})
