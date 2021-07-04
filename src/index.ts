6b95506d130c99e1b46c7e042946d0f4ee7be971
Ivan Galvagno <ivan.galvagno@gmail.com>
2021-07-04 13:17:57 -0300

    Init Project

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import "reflect-metadata";
+import {createConnection} from "typeorm";
+import * as express from "express";
+import * as bodyParser from "body-parser";
+import {Request, Response} from "express";
+import {Routes} from "./routes";
